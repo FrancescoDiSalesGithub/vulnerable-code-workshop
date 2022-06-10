d8a71e350e10606db4e22288baeef37e4bbf33ac
FrancescoDiSalesGithub <f.disales@hotmail.com>
2022-06-11 00:52:53 +0200

    adding init Host Header Injection

---
+++ HostHeaderInjection/src/main/java/com/vulnerableworkshop/HostHeaderInjection/HostHeaderInjectionApplication.java
@@ -0,0 +1,13 @@
+package com.vulnerableworkshop.HostHeaderInjection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HostHeaderInjectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HostHeaderInjectionApplication.class, args);
+	}
+
+}
---
+++ HostHeaderInjection/src/test/java/com/vulnerableworkshop/HostHeaderInjection/HostHeaderInjectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vulnerableworkshop.HostHeaderInjection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HostHeaderInjectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
