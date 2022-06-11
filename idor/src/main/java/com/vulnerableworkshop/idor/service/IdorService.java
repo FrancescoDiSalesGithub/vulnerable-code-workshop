4434490604068e37196fd6d190de5ebcbe7d921c
FrancescoDiSalesGithub <f.disales@hotmail.com>
2022-06-11 17:53:51 +0200

    adding service idor

--- idor/src/main/java/com/vulnerableworkshop/idor/service/IdorService.java
+++ idor/src/main/java/com/vulnerableworkshop/idor/service/IdorService.java
@@ -2,6 +2,8 @@ package com.vulnerableworkshop.idor.service;
 
 import org.springframework.stereotype.Service;
 
+import com.vulnerableworkshop.idor.model.IdorDocumentModel;
+
 @Service
 public class IdorService 
 {
@@ -9,7 +11,12 @@ public class IdorService
 	public String doIdor(String query)
 	{
 		if(query.equals("10"))
-			return "that's how idor works with this id there's my secret: document secret";
+		{
+			IdorDocumentModel modelSecret = new IdorDocumentModel();
+			modelSecret.setMessage("{watch for index}");
+			return "that's how idor works with this id there's my secret: "+modelSecret.getMessage();
+		}
+			
 		
 		return "document number: "+query;
 	}
